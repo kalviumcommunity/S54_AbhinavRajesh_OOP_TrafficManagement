ee2ef52ec19e16a3c2f8e6038aa331879f5844c9
Abhinav Rajesh <abhinav.rajesh@kalvium.community>
2024-11-18 13:25:48 +0530

    abstract class, virtual function

--- main.java
+++ main.java
@@ -1,111 +1,78 @@
-import java.util.*;
-
-// MotorVehicle Class
-class MotorVehicle {
+// Abstract Class
+abstract class BaseVehicle {
     protected String engineType;
 
-    public MotorVehicle(String engineType) {
+    public BaseVehicle(String engineType) {
         this.engineType = engineType;
     }
 
+    // Abstract Method (Pure Virtual Function)
+    public abstract void displayInfo();
+
+    // Common Method
     public void startEngine() {
         System.out.println("Engine started for " + engineType + " vehicle.");
     }
 }
 
 // Vehicle Class
-class Vehicle extends MotorVehicle {
-    // Static variable
+class Vehicle extends BaseVehicle {
     private static int totalVehicles = 0;
     private static int maxSpeedLimit = 120;
-
-    // Private members
     private String type;
     private int speed;
 
-    // Constructor
     public Vehicle(String type, int speed, String engineType) {
         super(engineType);
-        this.setType(type);
-        this.setSpeed(speed);
+        this.type = type;
+        this.speed = Math.min(speed, maxSpeedLimit);
         totalVehicles++;
     }
 
-    // Default constructor
     public Vehicle() {
         this("Unknown", 0, "Unknown");
     }
 
-    // Method Overloading for accelerate
-    public void accelerate(int increase) {
-        this.setSpeed(this.speed + increase);
+    // Overloaded
+    public Vehicle accelerate(int increase) {
+        this.speed = Math.min(this.speed + increase, maxSpeedLimit);
         System.out.println("New Speed: " + this.speed + " km/h");
+        return this;
     }
 
-    // Overloaded accelerate method (string message)
-    public void accelerate(String message) {
+    public Vehicle accelerate(String message) {
         System.out.println("Message: " + message);
+        return this;
     }
 
-    // Accelerate with both integer and string parameters
-    public void accelerate(int increase, String message) {
-        this.setSpeed(this.speed + increase);
-        System.out.println("New Speed: " + this.speed + " km/h, " + message);
+    public Vehicle accelerate(int increase, String message) {
+        this.accelerate(increase);
+        System.out.println("Message: " + message);
+        return this;
     }
 
-    // Manual cleanup method
-    public void cleanup() {
-        totalVehicles--;
-        System.out.println("Vehicle of type " + this.type + " cleaned up. Total vehicles: " + totalVehicles);
+    @Override
+    public void displayInfo() {
+        System.out.println("Vehicle Type: " + type);
+        System.out.println("Speed: " + speed + " km/h");
+        System.out.println("Engine Type: " + engineType);
+        System.out.println("Max Speed Limit: " + maxSpeedLimit + " km/h");
+        System.out.println("Total Vehicles: " + totalVehicles);
     }
 
-    // Static Getter
+    // Static Methods
     public static int getTotalVehicles() {
         return totalVehicles;
     }
 
-    // Getter and Setter
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        if (type != null && !type.isEmpty()) {
-            this.type = type;
-        } else {
-            this.type = "Unknown";
-        }
-    }
-
-    // Getter and Setter
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        if (speed >= 0 && speed <= maxSpeedLimit) {
-            this.speed = speed;
-        } else {
-            this.speed = maxSpeedLimit;
-        }
-    }
-
-    // Display Info method
-    public void displayInfo() {
-        System.out.println("Vehicle Type: " + this.getType());
-        System.out.println("Speed: " + this.getSpeed() + " km/h");
-        System.out.println("Engine Type: " + this.engineType);
-        System.out.println("Max Speed Limit: " + maxSpeedLimit + " km/h");
-        System.out.println("Total Vehicles: " + getTotalVehicles());
-    }
-
-    // Static Setter and Getter
     public static void setMaxSpeedLimit(int maxSpeed) {
         maxSpeedLimit = maxSpeed;
     }
 
-    public static int getMaxSpeedLimit() {
-        return maxSpeedLimit;
+    // Cleanup Method
+    public void cleanup() {
+        totalVehicles--;
+        System.out.println("Vehicle of type " + type + " cleaned up. Total vehicles: " + totalVehicles);
     }
 }
 
@@ -129,90 +96,35 @@ class ElectricVehicle extends Vehicle {
     }
 }
 
-// TrafficLight Class
-class TrafficLight {
-    private String color;
-
-    public TrafficLight(String initialColor) {
-        this.setColor(initialColor);
-    }
-
-    public TrafficLight() {
-        this("Red");
-    }
-
-    // Getter and Setter
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        if (color.equals("Red") || color.equals("Green") || color.equals("Yellow")) {
-            this.color = color;
-        } else {
-            this.color = "Red";
-        }
-    }
-
-    public void displayStatus() {
-        System.out.println("Traffic Light Color: " + this.getColor());
-    }
-
-    public TrafficLight changeColor(String newColor) {
-        this.setColor(newColor);
-        System.out.println("New Traffic Light Color: " + this.getColor());
-        return this;
-    }
-}
-
-// Simulation Class
-class Simulation {
-    private Vehicle vehicle;
-    private TrafficLight trafficLight;
-
-    public Simulation(Vehicle vehicle, TrafficLight trafficLight) {
-        this.vehicle = vehicle;
-        this.trafficLight = trafficLight;
-    }
-
-    public void run() {
-        System.out.println("Simulation Started:");
-        vehicle.displayInfo();
-        trafficLight.displayStatus();
-
-        trafficLight.changeColor("Green");
-        
-        // Calling the overloaded methods
-        vehicle.accelerate(20);
-        vehicle.accelerate("Speeding up!");
-        vehicle.accelerate(30, "Accelerating more quickly!");
-    }
-}
-
 // Main Class
 public class Main {
     public static void main(String[] args) {
-        Vehicle[] vehicles = new Vehicle[3];
+        BaseVehicle[] vehicles = new BaseVehicle[3];
         vehicles[0] = new Vehicle("Car", 60, "Petrol");
         vehicles[1] = new Vehicle();
         vehicles[2] = new ElectricVehicle("Electric Car", 80, 100);
 
-        for (int i = 0; i < vehicles.length; i++) {
-            System.out.println("Simulating Vehicle " + (i + 1) + ":");
+        for (BaseVehicle vehicle : vehicles) {
+            System.out.println("Simulating Vehicle:");
+            vehicle.startEngine();
+            vehicle.displayInfo();
 
-            TrafficLight streetLight = new TrafficLight("Red");
-            Simulation citySimulation = new Simulation(vehicles[i], streetLight);
-            citySimulation.run();
-
-            if (vehicles[i] instanceof ElectricVehicle) {
-                ((ElectricVehicle) vehicles[i]).chargeBattery();
+            if (vehicle instanceof Vehicle) {
+                ((Vehicle) vehicle).accelerate(20, "Speeding up!");
             }
+
+            if (vehicle instanceof ElectricVehicle) {
+                ((ElectricVehicle) vehicle).chargeBattery();
+            }
+
             System.out.println();
         }
 
-        // Manual Cleanup
-        for (Vehicle vehicle : vehicles) {
-            vehicle.cleanup();
+        // Cleanup Vehicles
+        for (BaseVehicle vehicle : vehicles) {
+            if (vehicle instanceof Vehicle) {
+                ((Vehicle) vehicle).cleanup();
+            }
         }
     }
 }
